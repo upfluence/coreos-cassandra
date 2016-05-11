d4782a7b3c2d86209c13f9ca0549e553ef769ca7
Alexis Montagne <alexis.montagne@gmail.com>
2016-05-10 17:31:36 -0700

    Build /etc/hosts file to make sure cassandra use the correct ip

--- image/run.sh
+++ image/run.sh
@@ -3,5 +3,6 @@
 /usr/bin/envtmpl -i ${CASSANDRA_HOME}/config/cassandra.yaml.tmpl -o ${CASSANDRA_HOME}/conf/cassandra.yaml
 
 echo "JVM_OPTS=\"\$JVM_OPTS -Djava.rmi.server.hostname=${CASSANDRA_IP_ADDRESS}\"" >> ${CASSANDRA_HOME}/conf/cassandra-env.sh
+echo "${CASSANDRA_IP_ADDRESS} `hostname`" > /etc/hosts
 
 /usr/share/cassandra/bin/cassandra -f
