58ec061b5659f296e276e37724f368b8a7c477ca
Alexis Montagne <alexis.montagne@gmail.com>
2017-11-03 12:38:22 -0400

    Add the promotheus prob in the image + bump of the version

--- image/run.sh
+++ image/run.sh
@@ -3,7 +3,7 @@
 /usr/bin/envtmpl -i ${CASSANDRA_HOME}/config/cassandra.yaml.tmpl -o ${CASSANDRA_HOME}/conf/cassandra.yaml
 
 echo "JVM_OPTS=\"\$JVM_OPTS -Djava.rmi.server.hostname=${CASSANDRA_IP_ADDRESS}\"" >> ${CASSANDRA_HOME}/conf/cassandra-env.sh
-echo "JVM_OPTS=\"\$JVM_OPTS -javaagent:${CASSANDRA_HOME}/lib/jolokia-jvm-agent.jar=port=7777,host=0.0.0.0\"" >> ${CASSANDRA_HOME}/conf/cassandra-env.sh
+echo "JVM_OPTS=\"\$JVM_OPTS -javaagent:${CASSANDRA_HOME}/lib/exporter.jar=0.0.0.0:7777:${CASSANDRA_HOME}/config/prometheus.yml\"" >> ${CASSANDRA_HOME}/conf/cassandra-env.sh
 echo "${CASSANDRA_IP_ADDRESS} `hostname`" > /etc/hosts
 
 /usr/share/cassandra/bin/cassandra -f
