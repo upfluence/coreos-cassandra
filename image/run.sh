2a2c2302a950fd26625f30475be2184618c69d44
Alexis Montagne <alexis.montagne@gmail.com>
2020-12-31 09:25:37 -0500

    image: Bump version of the prometheus exporter

--- image/run.sh
+++ image/run.sh
@@ -4,7 +4,7 @@
 /usr/bin/envtmpl -i ${CASSANDRA_HOME}/config/logback.xml.tmpl -o ${CASSANDRA_HOME}/conf/logback.xml
 
 echo "JVM_OPTS=\"\$JVM_OPTS -Djava.rmi.server.hostname=${CASSANDRA_IP_ADDRESS}\"" >> ${CASSANDRA_HOME}/conf/cassandra-env.sh
-echo "JVM_OPTS=\"\$JVM_OPTS -javaagent:${CASSANDRA_HOME}/lib/exporter.jar=0.0.0.0:7777:${CASSANDRA_HOME}/config/prometheus.yml\"" >> ${CASSANDRA_HOME}/conf/cassandra-env.sh
+echo "JVM_OPTS=\"\$JVM_OPTS -javaagent:${CASSANDRA_HOME}/lib/exporter.jar=7777:${CASSANDRA_HOME}/config/prometheus.yml\"" >> ${CASSANDRA_HOME}/conf/cassandra-env.sh
 echo "${CASSANDRA_IP_ADDRESS} `hostname`" > /etc/hosts
 
 /usr/share/cassandra/bin/cassandra -f
